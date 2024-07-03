a98b8f35aa87c7d5f493cb59f4e0b3708ce7946d
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2024-07-03 20:36:40 +0200

    Support evaluations sortings

--- app/models/decidim/internal_evaluation/internal_evaluation.rb
+++ app/models/decidim/internal_evaluation/internal_evaluation.rb
@@ -11,6 +11,26 @@ module Decidim
       belongs_to :author, foreign_key: "decidim_author_id", class_name: "Decidim::User"
 
       translatable_fields :body
+
+      class << self
+        def sort_by_state_asc
+          sort_by_state("ASC")
+        end
+
+        def sort_by_state_desc
+          sort_by_state("DESC")
+        end
+
+        private
+
+        def sort_by_state(keyword)
+          return if %w(ASC DESC).exclude?(keyword)
+
+          states = Decidim::Proposals::ProposalState.arel_table
+          field = Arel::Nodes::InfixOperation.new("->>", states[:title], Arel::Nodes.build_quoted(I18n.locale))
+          joins(:internal_state).order(Arel::Nodes::InfixOperation.new("", field, Arel.sql(keyword)))
+        end
+      end
     end
   end
 end
