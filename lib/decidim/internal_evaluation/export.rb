4135ae9696493141206d95d2927511f7dc3d36bf
Maxim Colls <collsmaxim@gmail.com>
2024-11-05 14:59:37 +0100

    Improve InternalEvaluation export and serializer

--- lib/decidim/internal_evaluation/export.rb
+++ lib/decidim/internal_evaluation/export.rb
@@ -12,20 +12,42 @@ module Decidim
       #
       # Returns an Arel::Relation with all the internal evaluations for that component and resource.
       def internal_evaluations_for_resource(resource_class, component, user)
-        participatory_space = component.participatory_space
-        collection = resource_class.not_hidden.published.where(component:)
-        user_is_valuator = participatory_space.user_roles(:valuator).where(user:).any?
-
-        filtered_collection = if user_is_valuator
-                                collection.with_valuation_assigned_to(user, participatory_space)
-                              else
-                                collection
-                              end
-
-        InternalEvaluation.where(proposal: filtered_collection.pluck(:id))
+        filtered_collection(resource_class, component, user).map do |resource|
+          resource.valuation_assignments.map do |assignment|
+            InternalEvaluation.find_or_initialize_by(proposal: resource, author: assignment.valuator)
+          end
+        end.flatten
       end
 
-      module_function :internal_evaluations_for_resource
+      # Internal: Returns the filtered collection for the given resource class, component and user.
+      #
+      # resource_class - The resource's Class
+      # component      - The component where the resource is scoped to.
+      # user           - The user that is requesting the export.
+      #
+      # Returns an Arel::Relation with the filtered collection.
+      def filtered_collection(resource_class, component, user)
+        return @collection if @collection
+
+        collection = resource_class
+          .not_hidden
+          .published
+          .where(component:)
+          .where.not(valuation_assignments_count: 0)
+
+        participatory_space = component.participatory_space
+        user_is_valuator = participatory_space.user_roles(:valuator).where(user:).any?
+
+        @collection = if user_is_valuator
+                        collection.with_valuation_assigned_to(user, participatory_space)
+                      else
+                        collection
+                      end
+
+        @collection
+      end
+
+      module_function :internal_evaluations_for_resource, :filtered_collection
     end
   end
 end
