232568159105f8ef42917a1310cd040e2d5fd571
Màxim Colls <collsmaxim@gmail.com>
2024-11-21 09:35:08 +0200

    Internal evaluations export improvements (#12)
    * Improve InternalEvaluation export and serializer
    
    * Defense code for absent valuator role
    
    * Add specs for the internal evaluation serializer
    
    * Add proposal id to internal evaluation export
    
    * Fix factory generation
    
    * Improve syntax
    
    ---------
    
    Co-authored-by: Eduardo Martinez Echevarria <eduardomech@gmail.com>

--- lib/decidim/internal_evaluation/export.rb
+++ lib/decidim/internal_evaluation/export.rb
@@ -12,20 +12,44 @@ module Decidim
       #
       # Returns an Arel::Relation with all the internal evaluations for that component and resource.
       def internal_evaluations_for_resource(resource_class, component, user)
-        participatory_space = component.participatory_space
-        collection = resource_class.not_hidden.published.where(component:)
-        user_is_valuator = participatory_space.user_roles(:valuator).where(user:).any?
+        filtered_collection(resource_class, component, user).map do |proposal|
+          proposal.valuation_assignments.map do |assignment|
+            author = assignment.valuator_role&.user
 
-        filtered_collection = if user_is_valuator
-                                collection.with_valuation_assigned_to(user, participatory_space)
-                              else
-                                collection
-                              end
+            next if author.blank?
 
-        InternalEvaluation.where(proposal: filtered_collection.pluck(:id))
+            InternalEvaluation.find_or_initialize_by(proposal:, author:)
+          end
+        end.flatten.compact
       end
 
-      module_function :internal_evaluations_for_resource
+      # Internal: Returns the filtered collection for the given resource class, component and user.
+      #
+      # resource_class - The resource's Class
+      # component      - The component where the resource is scoped to.
+      # user           - The user that is requesting the export.
+      #
+      # Returns an Arel::Relation with the filtered collection.
+      def filtered_collection(resource_class, component, user)
+        @filtered_collection ||= begin
+          collection = resource_class
+                       .not_hidden
+                       .published
+                       .where(component:)
+                       .where.not(valuation_assignments_count: 0)
+
+          participatory_space = component.participatory_space
+          user_is_valuator = participatory_space.user_roles(:valuator).where(user:).any?
+
+          if user_is_valuator
+            collection.with_valuation_assigned_to(user, participatory_space)
+          else
+            collection
+          end
+        end
+      end
+
+      module_function :internal_evaluations_for_resource, :filtered_collection
     end
   end
 end
