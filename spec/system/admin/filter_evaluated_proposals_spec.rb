3c6715fa80b36d098a915f33796fbd920821dc83
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2025-03-20 19:33:00 +0100

    Replace valuator for evaluator and valuation for evaluation

--- spec/system/admin/filter_evaluated_proposals_spec.rb
+++ spec/system/admin/filter_evaluated_proposals_spec.rb
@@ -13,16 +13,16 @@ describe "Admin filters evaluated proposals" do
   let(:resource_controller) { Decidim::Proposals::Admin::ProposalsController }
 
   let!(:user) { create(:user, :admin, :confirmed, organization:) }
-  let!(:valuator_user) { create(:user, organization:) }
+  let!(:evaluator_user) { create(:user, organization:) }
   # let!(:user) { admin }
-  let!(:valuator_role) { create(:participatory_process_user_role, role: :valuator, user: valuator_user, participatory_process:) }
+  let!(:evaluator_role) { create(:participatory_process_user_role, role: :evaluator, user: evaluator_user, participatory_process:) }
   let!(:another_user) { create(:user, organization:) }
-  let!(:another_valuator_role) { create(:participatory_process_user_role, role: :valuator, user: another_user, participatory_process:) }
-  let!(:user_valuation_assignment) { create(:valuation_assignment, proposal: assigned_proposal_evaluated, valuator_role:) }
-  let!(:user_valuation_assignment2) { create(:valuation_assignment, proposal: assigned_proposal_not_evaluated, valuator_role:) }
-  let!(:another_user_valuation_assignment) { create(:valuation_assignment, proposal: assigned_proposal_evaluated, valuator_role: another_valuator_role) }
-  let!(:another_user_valuation_assignment2) { create(:valuation_assignment, proposal: assigned_proposal_not_evaluated, valuator_role: another_valuator_role) }
-  let!(:valuator_user_evaluation) { create(:internal_evaluation, proposal: assigned_proposal_evaluated, author: valuator_user) }
+  let!(:another_evaluator_role) { create(:participatory_process_user_role, role: :evaluator, user: another_user, participatory_process:) }
+  let!(:user_evaluation_assignment) { create(:evaluation_assignment, proposal: assigned_proposal_evaluated, evaluator_role:) }
+  let!(:user_evaluation_assignment2) { create(:evaluation_assignment, proposal: assigned_proposal_not_evaluated, evaluator_role:) }
+  let!(:another_user_evaluation_assignment) { create(:evaluation_assignment, proposal: assigned_proposal_evaluated, evaluator_role: another_evaluator_role) }
+  let!(:another_user_evaluation_assignment2) { create(:evaluation_assignment, proposal: assigned_proposal_not_evaluated, evaluator_role: another_evaluator_role) }
+  let!(:evaluator_user_evaluation) { create(:internal_evaluation, proposal: assigned_proposal_evaluated, author: evaluator_user) }
   let!(:another_user_evaluation) { create(:internal_evaluation, proposal: assigned_proposal_evaluated, author: another_user) }
 
   include_context "when managing a component as an admin"
@@ -31,7 +31,7 @@ describe "Admin filters evaluated proposals" do
   context "when filtering by evaluation status" do
     before { visit_component_admin }
 
-    context "when admin is not valuator" do
+    context "when admin is not evaluator" do
       it "does not show the evaluation filter" do
         within(".filters__section") do
           find_link("Filter").hover
@@ -41,9 +41,9 @@ describe "Admin filters evaluated proposals" do
         end
       end
 
-      context "when admin filters by valuator" do
+      context "when admin filters by evaluator" do
         before do
-          apply_filter "Assigned to valuator", another_user.name
+          apply_filter "Assigned to evaluator", another_user.name
         end
 
         it "shows the evaluation filter" do
@@ -71,8 +71,8 @@ describe "Admin filters evaluated proposals" do
       # end
     end
 
-    context "when admin is valuator" do
-      let!(:valuator_user) { user }
+    context "when admin is evaluator" do
+      let!(:evaluator_user) { user }
 
       it "shows the evaluation filter" do
         within(".filters__section") do
@@ -83,7 +83,7 @@ describe "Admin filters evaluated proposals" do
         end
       end
 
-      context "and filters by admin as valuator" do
+      context "and filters by admin as evaluator" do
         it_behaves_like "a filtered collection", options: "Evaluation", filter: "Evaluated" do
           let(:in_filter) { translated(assigned_proposal_evaluated.title) }
           let(:not_in_filter) { translated(assigned_proposal_not_evaluated.title) }
@@ -95,9 +95,9 @@ describe "Admin filters evaluated proposals" do
         end
       end
 
-      context "when admin filters by valuator" do
+      context "when admin filters by evaluator" do
         before do
-          apply_filter "Assigned to valuator", another_user.name
+          apply_filter "Assigned to evaluator", another_user.name
         end
 
         it_behaves_like "a filtered collection", options: "Evaluation", filter: "Evaluated" do
