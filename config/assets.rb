c8b70a320c693273c56a57811efe528e29f75710
Fran Bolívar (FBO) <francisco.bolivar.lupianez@gmail.com>
2026-01-20 12:25:29 +0100

    Fix deprecation warning (#23)

--- config/assets.rb
+++ config/assets.rb
@@ -2,8 +2,8 @@
 
 base_path = File.expand_path("..", __dir__)
 
-Decidim::Webpacker.register_path("#{base_path}/app/packs")
-Decidim::Webpacker.register_entrypoints(
+Decidim::Shakapacker.register_path("#{base_path}/app/packs")
+Decidim::Shakapacker.register_entrypoints(
   decidim_internal_evaluation: "#{base_path}/app/packs/entrypoints/decidim_internal_evaluation.js"
 )
-Decidim::Webpacker.register_stylesheet_import("stylesheets/decidim/internal_evaluation/internal_evaluation")
+Decidim::Shakapacker.register_stylesheet_import("stylesheets/decidim/internal_evaluation/internal_evaluation")
