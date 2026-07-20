8bf5098883b5ba502ff020e051a8b28d80bc2205
Fran Bolívar <francisco.bolivar@nazaries.com>
2026-07-20 10:27:45 +0200

    Upgrade decidim to 0.32

--- lib/decidim/internal_evaluation/version.rb
+++ lib/decidim/internal_evaluation/version.rb
@@ -4,11 +4,11 @@ module Decidim
   # This holds the decidim-meetings version.
   module InternalEvaluation
     def self.version
-      "0.0.3"
+      "0.0.4"
     end
 
     def self.decidim_version
-      "~> 0.31.0"
+      "~> 0.32.0"
     end
   end
 end
