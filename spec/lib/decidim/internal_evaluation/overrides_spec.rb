8352585271f71177eda531e689b06a73beb268be
Fran Bolívar (FBO) <francisco.bolivar.lupianez@gmail.com>
2026-09-11 10:07:07 +0200

    Upgrade decidim to 0.32 (#25)
    * Upgrade decidim to 0.32
    
    * Lint
    
    * Fix tests
    
    * Migrate from data-component="accordion" to data-controller="accordion"
    
    * Fix development app generation

--- spec/lib/decidim/internal_evaluation/overrides_spec.rb
+++ spec/lib/decidim/internal_evaluation/overrides_spec.rb
@@ -9,9 +9,9 @@ checksums = [
     package: "decidim-proposals",
     files: {
       "/app/views/decidim/proposals/admin/proposals/_proposal-tr.html.erb" => "608af89f7bfa800fe2e3c853f8e2ace0",
-      "/app/views/decidim/proposals/admin/proposals/show.html.erb" => "541d1f188e12c6c6ccf18d8908165d81",
-      "/app/controllers/decidim/proposals/admin/proposals_controller.rb" => "e4c0f7d53474d75a9f9a091970995abb",
-      "/app/controllers/decidim/proposals/admin/proposal_states_controller.rb" => "46d227d4b80c177ee4bf50eba003d008",
+      "/app/views/decidim/proposals/admin/proposals/show.html.erb" => "8beac85dc41b860a2cc83efa6fa97e42",
+      "/app/controllers/decidim/proposals/admin/proposals_controller.rb" => "f815697e34a2a24ce20696e7f6c46d4f",
+      "/app/controllers/decidim/proposals/admin/proposal_states_controller.rb" => "23cdaa84a8951ed053e374b6bc5b3428",
       "/app/commands/decidim/proposals/admin/unassign_proposals_from_evaluator.rb" => "8dc24ee1ae681b57c475e873f5d0347f"
     }
   }
@@ -22,7 +22,9 @@ describe "Overriden files", type: :view do
     spec = Gem::Specification.find_by_name(item[:package])
     item[:files].each do |file, signature|
       it "#{spec.gem_dir}#{file} matches checksum" do
-        expect(md5("#{spec.gem_dir}#{file}")).to eq(signature)
+        gem_dir = Gem::Specification.find_by_name(item[:package]).gem_dir
+
+        expect(md5("#{gem_dir}#{file}")).to eq(signature)
       end
     end
   end
